464fcd2d584f21071a88e1eaba03340781e43f03
AntwanTx <amunoz021@gmail.com>
2023-09-16 14:27:53 -0500

    added solution 5

--- solution5.py
+++ solution5.py
@@ -1,1 +1,15 @@
 # Write code for algorithm 5 below
+
+def pal_check(string):
+    # base case:
+    if len(string) == 1 or len(string) == 0:
+        return True
+    
+    # recursive case:
+    else:
+        return string[0] == string[-1] and pal_check(string[1:-1])
+            
+
+
+
+print(pal_check("tacocat"))
